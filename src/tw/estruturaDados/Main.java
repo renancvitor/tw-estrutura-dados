e0121fbff6949ac7b06d18be785ac4616ff124ac
renancvitor <renan.vitor.cm@gmail.com>
2025-02-18 18:25:23 -0300

    Memória JVM

--- src/tw/estruturaDados/Main.java
+++ src/tw/estruturaDados/Main.java
@@ -1,7 +1,50 @@
 package tw.estruturaDados;
 
+import estruturaDadosModelos.Pessoa;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite a opção deseja: ");
+        System.out.println("1. Gerenciamento de memória");
+
+        int opcao = scanner.nextInt();
+
+        switch (opcao) {
+            case 1:
+                fazerGerenciamentoMemoria();
+                break;
+        }
+
+        scanner.close();
     }
+
+    private static void fazerGerenciamentoMemoria() {
+        int a = 3;
+        System.out.println(a);
+        int b = a;
+        System.out.println(b);
+        b = 2;
+        System.out.println("______________________");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a == b);
+        System.out.println("______________________");
+
+        Pessoa p1 = new Pessoa(1, "TreinaWeb");
+        System.out.println(p1.toString());
+        Pessoa p2 = new Pessoa(1, "TreinaWeb");
+        System.out.println(p2.toString());
+        System.out.println("______________________");
+
+        // p2.setNome("TreinaWeb modificado");
+        System.out.println(p1.toString());
+        System.out.println(p2.toString());
+        System.out.println(p1.equals(p2));
+    }
+
 }
