46ba5ae91e9cbbc60eefc5997f77f03f43342272
renancvitor <renan.vitor.cm@gmail.com>
2025-02-18 23:35:56 -0300

    Vetores

--- src/tw/estruturaDados/Main.java
+++ src/tw/estruturaDados/Main.java
@@ -11,12 +11,16 @@ public class Main {
 
         System.out.println("Digite a opção deseja: ");
         System.out.println("1. Gerenciamento de memória");
+        System.out.println("2. Vetores");
 
         int opcao = scanner.nextInt();
 
         switch (opcao) {
             case 1:
                 fazerGerenciamentoMemoria();
+                break;
+            case 2:
+                fazerVetor();
                 break;
         }
 
@@ -47,4 +51,12 @@ public class Main {
         System.out.println(p1.equals(p2));
     }
 
+    private static void fazerVetor() {
+
+        Pessoa[] vetorPessoas = new Pessoa[3];
+        vetorPessoas[0] = new Pessoa(1, "TreinaWeb no Vetor");
+        System.out.println(vetorPessoas[0].getNome());
+        int[] vetorInteiro = new int[3];
+        System.out.println(vetorInteiro[0]);
+    }
 }
