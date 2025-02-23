83f9f212881b573d64ae7c80bbc031240c5ea44a
renancvitor <renan.vitor.cm@gmail.com>
2025-02-23 12:05:09 -0300

    Método posição

--- src/estruturaDadosVetores/Vetor.java
+++ src/estruturaDadosVetores/Vetor.java
@@ -3,12 +3,27 @@ package estruturaDadosVetores;
 public class Vetor<T> {
 
     private Object[] elementos;
+    private int posicao;
 
     public Vetor(int capacidade) {
         this.elementos = new Object[capacidade];
+        this.posicao = 0
+    }
+
+    public Vetor() {
+        this.elementos = new Object[3];
+        this.posicao = 0;
+    }
+
+    public void inserir(T elemento) {
+        this.elementos[this.posicao] = elemento;
+        this.posicao++;
     }
 
     public void inserirEm(int posicao, T elemento) {
+        if (posicao < this.elementos.length) {
+            throw new IllegalArgumentException(String.format("A posição [%d] é inválida.", posicao));
+        }
         this.elementos[posicao] = elemento;
     }
 
