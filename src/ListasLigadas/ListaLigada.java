6822c355d73eccec9271f4a8a1262f391d8caab3
renancvitor <renan.vitor.cm@gmail.com>
2025-02-24 15:22:10 -0300

    Classe ListaLigada

---
+++ src/ListasLigadas/ListaLigada.java
@@ -0,0 +1,15 @@
+package ListasLigadas;
+
+public class ListaLigada<T> {
+
+    private No<T> primeiroNo;
+    private No<T> ultimoNo;
+    private int tamanho;
+
+    public ListaLigada() {
+        this.primeiroNo = null;
+        this.ultimoNo = null;
+    }
+
+    
+}
