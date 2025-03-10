506b7c3b534c5b338bf009b346a3eda73e338cf3
renancvitor <renan.vitor.cm@gmail.com>
2025-03-10 17:07:13 -0300

    Implementação base Fila

---
+++ src/estruturaDadosFilas/Fila.java
@@ -0,0 +1,25 @@
+package estruturaDadosFilas;
+
+import ListasLigadas.ListaLigada;
+
+public class Fila<T> {
+
+    private ListaLigada<T> elementos;
+
+    public Fila() {
+        this.elementos = new ListaLigada();
+    }
+
+    public void enfileirar(T elemento) {
+
+    }
+
+    public T desenfileirar() {
+
+    }
+
+    public boolean estaVazia() {
+        
+    }
+
+}
