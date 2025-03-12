cdf644b10390db6a08afa4379723751f3b628e90
renancvitor <renan.vitor.cm@gmail.com>
2025-03-11 23:31:44 -0300

    Aproveitamento métodos

--- src/estruturaDadosSets/Set.java
+++ src/estruturaDadosSets/Set.java
@@ -1,41 +1,49 @@
 package estruturaDadosSets;
 
+import ListasLigadas.ListaLigada;
+
 public class Set<T> {
 
-    public void inserir(T elemento) {
+    private ListaLigada<T> elementos;
 
+    public Set() {
+        this.elementos = new ListaLigada<T>();
+    }
+
+    public void inserir(T elemento) {
+        this.elementos.inserir(elemento);
     }
 
     public void inserirEm(int posicao, T elemento) {
-
+        this.elementos.inserirEM(posicao, elemento);
     }
 
     public T recuperar(int posicao) {
-        return null;
+        return this.elementos.recuperar(posicao);
     }
 
     public boolean estaVazia() {
-        return false;
+        return this.elementos.estaVazia();
     }
 
     public int tamanho() {
-        return 0;
+        return this.elementos.tamanho();
     }
 
     public boolean contem(T elemento) {
-        return false;
+        return this.elementos.contem(elemento)
     }
 
     public int indice(T elemento) {
-        return 0;
+        return this.elementos.indice(elemento);
     }
 
     public void remover(int posicao) {
-
+        this.elementos.remover(posicao);
     }
 
     public void remover(T elemento) {
-
+        this.elementos.remover(elemento);
     }
     
 }
